7e55663f34f9b6baf5e48fa46b5a79068ee5785b
ignaciocisterna <ignaciocisternabambach@gmail.com>
2026-01-27 09:32:38 -0300

    Update __init__.py
    ajuste a versión

--- xrf_toolbox/__init__.py
+++ xrf_toolbox/__init__.py
@@ -14,9 +14,10 @@ from .metrics import exportar_reporte_pdf
 
 
 # Versión del paquete 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 # Controlar qué se importa con "from xrf_toolbox import *"
 __all__ = ["XRFDeconv", "recortar_espectro", "get_Xray_info", "detectar_elementos",
             "graficar_deteccion_preliminar", "graficar_ajuste", "generar_reporte_completo",
+
             "exportar_reporte_pdf"]
