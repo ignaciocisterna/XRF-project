824074700f4ff8476ca33761c5e504ba3cf02d73
ignaciocisterna <ignaciocisternabambach@gmail.com>
2026-02-13 13:18:01 -0300

    Update deconv.py

--- xrf_toolbox/deconv.py
+++ xrf_toolbox/deconv.py
@@ -120,7 +120,8 @@ class XRFDeconv:
             if not core.is_excitable(xl.SymbolToAtomicNumber(elemento), familia, self.config):
                 return 0
             # 2. ¿Tiene líneas en el rango de energía actual?
-            info = core.get_Xray_info(elemento, families=(familia,), config_anode=self.config.anode)
+            familia_tuple = tuple(familia) if isinstance(familia, list) else (familia,
+            info = core.get_Xray_info(elemento, families=familia_tuple, config_anode=self.config.anode)
             for line_data in info.values():
                 if e_min <= line_data['energy'] <= e_max:
                     return 1
@@ -672,6 +673,7 @@ class XRFDeconv:
                 
         df = pd.DataFrame(res).fillna("-")
         return df
+
 
 
 
